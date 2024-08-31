56670c7a4e586c7c04f9aa6de71227063f63847b
hardikarora25 <hardikarora5000@gmail.com>
2024-08-31 19:16:54 +0530

    my first commit

---
+++ print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+class print{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("my first java program");
+    }
+}
