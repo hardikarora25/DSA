1d863bd694e30d70fde9de011b471e0ae4a2049c
hardikarora25 <hardikarora5000@gmail.com>
2024-09-20 00:05:16 +0530

    added something new

---
+++ Array/hardik.java
@@ -0,0 +1,13 @@
+package Array;
+
+import java.util.Scanner;
+
+public class hardik {
+    public static void main(String[] args) {
+        System.out.println("hey hello");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextline());
+
+    }
+    
+}
---
+++ Array/primitives.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class primitives {
+    public static void main(String[] args) {
+        int rollno = 64;
+        char letter = 'rg';
+        float marks = 98.67f;
+        double largeDecimalnos = 5657656.656;
+        long largeinteger = 9898989890L;
+        boolean check = false;
+        
+
+        
+
+    }
+    
+}
