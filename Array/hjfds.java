7d6f922ad0b61042cee4f4ca4e46845589850480
hardikarora25 <hardikarora5000@gmail.com>
2024-09-21 01:14:23 +0530

    hey added this

---
+++ Array/hjfds.java
@@ -0,0 +1,21 @@
+package Array;
+
+import java.util.Scanner;
+
+public class hjfds {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        int summ = num1 + num2;
+        System.out.println("the sum is " + summ);
+
+
+
+        
+        
+        
+
+    }
+    
+}
--- Array/niggga.java
+++ Array/niggga.java
@@ -2,7 +2,7 @@ package Array;
 
 import java.util.Scanner;
 
-public class inputs {
+public class niggga {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("please enter some input ");
