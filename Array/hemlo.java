9421184b4e3fdcb1c6f146058b95d520be7406ec
hardikarora25 <hardikarora5000@gmail.com>
2024-09-21 23:32:53 +0530

    added loops

---
+++ Array/hemlo.java
@@ -0,0 +1,24 @@
+package Array;
+
+public class hemlo {
+    public static void main(String[] args) {
+        
+    
+    int a = 10;
+    if (a == 90) {
+        System.out.println("hello world");
+    
+    }
+
+    int counts = 1;
+    while(counts != 6){
+        System.out.println(counts);
+        counts++;
+        
+    }
+    // for loop 
+    for(int count = 1 ; count != 5; count++){
+        System.out.println(count);
+    }
+}
+}
