615b44cd91ad2502914eda8ba3bed548e6f1c59f
hardikarora25 <hardikarora5000@gmail.com>
2024-10-04 16:00:56 +0530

    bhhhh

--- Array/HEYA.java
+++ Array/HEYA.java
@@ -3,6 +3,7 @@ package Array;
 public class HEYA {
     public static void main(String[] args) {
         System.out.println("starting it");
+        System.out.println("lolo");
     }
     
 }
---
+++ Array/lol/hR.java
@@ -0,0 +1,8 @@
+public package Array.lol;
+
+public class hR { public static void main(String[] args) {
+    System.out.println("hryyyyyyy");
+}
+
+    
+}
