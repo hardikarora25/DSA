c0a10fc7186fdc5ad12cd4d897be19fb9be99a36
hardikarora25 <hardikarora5000@gmail.com>
2024-10-21 21:55:13 +0530

    a new commit

---
+++ Array/LOOPS/forloop.java
@@ -0,0 +1,20 @@
+package Array.LOOPS;
+
+import java.util.Scanner;
+
+public class forloop {
+    public static void main(String[] args) {
+        for (int num = 1; num <= 5; num+=1) {
+            System.out.println(num);
+        }
+        try (// printing no.s 1 to n 
+        Scanner in = new Scanner(System.in)) {
+            int n = in.nextInt();
+            
+            for (int num = 0;num <= n ;num++ ){
+                System.out.println(num);
+            }
+        }
+    }
+    
+}
