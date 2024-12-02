1bec7372040e3035116d1cf53e542e2651a66488
hardikarora25 <hardikarora5000@gmail.com>
2024-12-02 17:39:52 +0530

    Starting methods

---
+++ Array/Method/Stringgg.java
@@ -0,0 +1,12 @@
+package Array.Method;
+
+public class Stringgg {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+    static String greet(){
+        String greeting = "hello world";
+        return greeting;
+    }
+}
---
+++ Array/Method/sumF.java
@@ -0,0 +1,14 @@
+package Array.Method;
+
+public class sumF {
+    public static void main(String[] args) {
+        int summm = sum1(90, 140);
+        System.out.println(summm);
+    }
+    
+    static int sum1(int a, int b){
+        int sum = a + b;
+        return sum;
+
+    }
+}
