bfbba2f8f7ce7753051e29e619b995115a39a913
hardikarora25 <hardikarora5000@gmail.com>
2025-02-06 10:32:59 +0530

    discipline will prevail

---
+++ Array/ARRAYquestions/tuf.java
@@ -0,0 +1,25 @@
+package Array.ARRAYquestions;
+public class tuf {
+ 
+  public static void main(String args[]) {
+ 
+    int hr1[] =  {2,5,1,3,0};
+    System.out.println("The Largest element in the array is: "+findLargestElement(hr1
+    ));
+ 
+    int hr2[] =  {8,10,5,7,9};
+    System.out.println("The Largest element in the array is: "+findLargestElement(hr2
+    ));
+  }
+  static int findLargestElement(int hrr[]) {
+    int max= hrr[0];
+    for (int i = 0; i < hrr.length; i++) {
+      if (hrr[i] > max) {
+        max= hrr[i];
+      }
+    }
+    return max;
+  }
+}
+
+
---
+++ Array/arraybasic.java
@@ -0,0 +1,13 @@
+package Array;
+public class arraybasic {
+    public static void main(String[] args) {
+        int[] intArray = new int[]{ 1,2,3,4,5,6,7,8,9,10 }; 
+        // accessing elements in the array 
+        for ( int i = 0 ; i < intArray.length ; i++)
+        System.out.println("element at index" + i + " : " + intArray[i] );
+
+
+
+    }
+    
+}
